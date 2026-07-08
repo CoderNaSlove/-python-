3b1ddfd29b38b37f664d5884eae82af00a996543
CoderNaSlove <zverevvalerij5@gmail.com>
2026-07-08 22:34:16 +0300

    теперь сервер отправляет два сообщения за один цикл

--- webserver.py
+++ webserver.py
@@ -5,13 +5,9 @@ import random
 import time
 import websockets
 
-am1 = 2
-ct1 = 23
-fr1 = 0.1
+am1, ct1, fr1 = 2, 23, 0.1
+am2, ct2, fr2 = 8, 50, 0.1
 
-am2 = 8
-ct2 = 50
-fr2 = 0.1
 
 
 async def handle_client(websocket):
@@ -20,23 +16,28 @@ async def handle_client(websocket):
     while True:
         current_time = time.time()
         
-
         val1 = ct1 + am1 * math.sin(fr1 * current_time)
-        val1 += random.gauss(0, 0.2)
+        val1 += random.gauss(0, 0.2)  
         
-
+        data_sensor_1 = {
+            "chartId": "sensor_1",
+            "value": round(val1, 1)
+        }
+        
+       
         val2 = ct2 + am2 * math.cos(fr2 * current_time)
-        val2 += random.gauss(0, 0.4)
+        val2 += random.gauss(0, 0.3)  
         
-
-        data = {
-            "sensor_1": round(val1, 1),
-            "sensor_2": round(val2, 1)
+        data_sensor_2 = {
+            "chartId": "sensor_2",
+            "value": round(val2, 1)
         }
 
+        
+        await websocket.send(json.dumps(data_sensor_1))
+        await websocket.send(json.dumps(data_sensor_2))
 
-        await websocket.send(json.dumps(data))
-
+        
         await asyncio.sleep(0.1)
 
 
